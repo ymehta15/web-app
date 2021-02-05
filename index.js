d357ac043f8f0dfa8bd0a771d6e6933594b9f88d
ymehta15 <64590803+ymehta15@users.noreply.github.com>
2021-02-05 22:57:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const logger = require("morgan");
 const path = require("path");
 const { createServer } = require("http");
 const { auth, requiresAuth } = require("express-openid-connect");
+const axios = require("axios").default;
 
 const {
   checkUrl,
