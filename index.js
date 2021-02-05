7354a6880ca9e5b9cf20735f3398e1b07d0a1e57
ymehta15 <64590803+ymehta15@users.noreply.github.com>
2021-02-05 16:03:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const path = require("path");
 const { createServer } = require("http");
-const { auth } = require("express-openid-connect");
+const { auth, requiresAuth } = require("express-openid-connect");
 
 const {
   checkUrl,
@@ -40,6 +40,7 @@ app.use(
 app.use(
  auth({
    secret: SESSION_SECRET,
+   authRequired: false,
    auth0Logout: true,
    baseURL: APP_URL,
  })
@@ -68,7 +69,7 @@ app.get("/", async (req, res) => {
 
 // 👇 add requiresAuth middlware to these private routes  👇
 
-app.get("/user", async (req, res) => {
+app.get("/user", requiresAuth(), async (req, res) => {
   res.render("user", {
     user: req.oidc && req.oidc.user,
     id_token: req.oidc && req.oidc.idToken,
@@ -77,7 +78,7 @@ app.get("/user", async (req, res) => {
   });
 });
 
-app.get("/expenses", async (req, res, next) => {
+app.get("/expenses", requiresAuth(),async (req, res, next) => {
   res.render("expenses", {
     user: req.oidc && req.oidc.user,
     expenses,
