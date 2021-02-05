abf40831609e56d0aedd26b9b6426dbe773fc7cf
ymehta15 <64590803+ymehta15@users.noreply.github.com>
2021-02-05 22:54:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,6 +43,13 @@ app.use(
    authRequired: false,
    auth0Logout: true,
    baseURL: APP_URL,
+   
+   // 👇 add this 👇
+   authorizationParams: {
+     response_type: "code id_token",
+     audience: "https://expenses-api",
+   },
+   // 👆 add this 👆
  })
 );
 
@@ -78,11 +85,24 @@ app.get("/user", requiresAuth(), async (req, res) => {
   });
 });
 
-app.get("/expenses", requiresAuth(),async (req, res, next) => {
-  res.render("expenses", {
-    user: req.oidc && req.oidc.user,
-    expenses,
-  });
+app.get("/expenses", requiresAuth(), async (req, res, next) => {
+ try {
+   // 👇 get the token from the request 👇
+   const { token_type, access_token } = req.oidc.accessToken;
+   // 👇 then send it as an authorization header 👇
+   const expenses = await axios.get(`${API_URL}/reports`, {
+     headers: {
+       Authorization: `${token_type} ${access_token}`,
+     },
+   });
+   // 👆 end of changes 👆
+   res.render("expenses", {
+     user: req.oidc && req.oidc.user,
+     expenses: expenses.data,
+   });
+ } catch (err) {
+   next(err);
+ }
 });
 
 // catch 404 and forward to error handler
